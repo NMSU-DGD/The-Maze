2e89d75120c34178ad588b77977bdd5d7ec67fd5
Ahmed Khalaf <ahmedkhalaf@Administrators-Mac-mini.local>
2016-11-15 21:38:51 -0700

    adding the door button code and texture

--- Project/Maze 2.0/Assets/Scripts/DoorButtonScript.cs
+++ Project/Maze 2.0/Assets/Scripts/DoorButtonScript.cs
@@ -3,19 +3,37 @@ using System.Collections;
 
 public class DoorButtonScript : MonoBehaviour {
 
-	// Use this for initialization
+
+	Movement2D player;
+
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
-		Movement2D player = otherCollider.gameObject.GetComponent<Movement2D>();
+		string keyName = gameObject.name;
+		player = otherCollider.gameObject.GetComponent<Movement2D>();
 		if (player != null) {
-			Destroy (transform.parent.gameObject);
+			if (keyName == "DoorButton")
+			{
+				GameObject.Find("ButtonDoor01").GetComponent<MeshRenderer>().enabled = false;
+				GameObject.Find("ButtonDoor01").GetComponent<BoxCollider2D>().enabled = false;
+			}
+
 		}
 
 	}
 
-	void Start()
+	void OnTriggerExit2D(Collider2D otherCollider)
 	{
-		// 2 - Limited time to live to avoid any leak
-		// Destroy(gameObject, 20); // 20sec
+		string keyName = gameObject.name;
+		player = otherCollider.gameObject.GetComponent<Movement2D>();
+		if (player != null) {
+			if (keyName == "DoorButton")
+			{
+				GameObject.Find("ButtonDoor01").GetComponent<MeshRenderer>().enabled = true;
+				GameObject.Find("ButtonDoor01").GetComponent<BoxCollider2D>().enabled = true;
+			}
+
+		}
+
 	}
 }
+
