bda632af7f344d7a2e9939117f0adaa723f698ad
Ahmed Khalaf <ahmedkhalaf@Administrators-Mac-mini.local>
2016-11-15 22:09:16 -0700

    Ahmad and Sultan: Fixed the drawing part

--- Project/Maze 2.0/Assets/Scripts/DrawLine.cs
+++ Project/Maze 2.0/Assets/Scripts/DrawLine.cs
@@ -7,6 +7,7 @@ public class DrawLine : MonoBehaviour {
 
 	private LineRenderer line;
 	private bool isMousePressed;
+
 	public List<Vector3> pointsList;
 	private Vector3 mousePos;
 	private Vector2 _offset;
@@ -26,7 +27,7 @@ public class DrawLine : MonoBehaviour {
 		line.SetVertexCount (0);
 		line.SetWidth (0.1f, 0.1f);
 		line.SetColors (Color.green, Color.green);
-		line.useWorldSpace = true;    
+		line.useWorldSpace = true;
 		isMousePressed = true;
 		pointsList = new List<Vector3> ();
 		//        renderer.material.SetTextureOffset(
@@ -38,78 +39,91 @@ public class DrawLine : MonoBehaviour {
 		//this.transform.position(pare
 		// If mouse button down, remove old line and set its color to green
 		if (Input.GetMouseButtonDown (0)) {
-//			_offset = eventData.position - new Vector2 (this.transform.position.x, this.transform.position.y);
-//			this.transform.position = eventData.position - _offset;
+			//			_offset = eventData.position - new Vector2 (this.transform.position.x, this.transform.position.y);
+			//			this.transform.position = eventData.position - _offset;
 			isMousePressed = true;
 			line.SetVertexCount (0);
-			pointsList.RemoveRange (0, pointsList.Count);
+			//pointsList.RemoveRange (0, pointsList.Count);
 			line.SetColors (Color.green, Color.green);
 		}
+
 		if (Input.GetMouseButtonUp (0)) {
+			Debug.Log("mouseup");
 			isMousePressed = false;
+			line.SetColors (Color.red, Color.red);
 		}
+
+		if (Input.GetMouseButtonDown (1)) {
+			line.SetVertexCount (0);
+			Debug.Log("right click");
+			pointsList.RemoveRange (0, pointsList.Count);
+		}
+
 		// Drawing line when mouse is moving(presses)
-		if (isMousePressed) {
+		else if (isMousePressed) {
 			mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			//mousePos = GameObject.FindWithTag("Planningmap")
 			//mousePos = GameObject.Find("Planningmap") (Input.mousePosition);
-			mousePos.z = 0;
+			mousePos.z = -2;
+			//mousePos.x = 1;
+			//mousePos.y = 1;
 			if (!pointsList.Contains (mousePos)) {
 				pointsList.Add (mousePos);
 				line.SetVertexCount (pointsList.Count);
 				line.SetPosition (pointsList.Count - 1, (Vector3)pointsList [pointsList.Count - 1]);
-				if (isLineCollide ()) {
-					isMousePressed = false;
-					line.SetColors (Color.red, Color.red);
-				}
+				//				if (isLineCollide ()) {
+				//					isMousePressed = false;
+				//					line.SetColors (Color.red, Color.red);
+				//				}
 			}
 		}
 	}
 	//    -----------------------------------    
 	// Following method checks is currentLine(line drawn by last two points) collided with line
 	//    -----------------------------------    
-	private bool isLineCollide ()
-	{
-		if (pointsList.Count < 2)
-			return false;
-		int TotalLines = pointsList.Count - 1;
-		myLine[] lines = new myLine[TotalLines];
-		if (TotalLines > 1) {
-			for (int i=0; i<TotalLines; i++) {
-				//lines [i].StartPoint = (Vector3)pointsList [i];
-				//lines [i].EndPoint = (Vector3)pointsList [i + 1];
-			}
-		}
-		for (int i=0; i<TotalLines-1; i++) {
-			myLine currentLine;
-			currentLine.StartPoint = (Vector3)pointsList [pointsList.Count - 2];
-			currentLine.EndPoint = (Vector3)pointsList [pointsList.Count - 1];
-			if (isLinesIntersect (lines [i], currentLine))
-				return true;
-		}
-		return false;
-	}
+	//	private bool isLineCollide ()
+	//	{
+	//		if (pointsList.Count < 2)
+	//			return false;
+	//		int TotalLines = pointsList.Count - 1;
+	//		myLine[] lines = new myLine[TotalLines];
+	//		if (TotalLines > 1) {
+	//			for (int i=0; i<TotalLines; i++) {
+	//				//lines [i].StartPoint = (Vector3)pointsList [i];
+	//				//lines [i].EndPoint = (Vector3)pointsList [i + 1];
+	//			}
+	//		}
+	//		for (int i=0; i<TotalLines-1; i++) {
+	//			myLine currentLine;
+	//			currentLine.StartPoint = (Vector3)pointsList [pointsList.Count - 2];
+	//			currentLine.EndPoint = (Vector3)pointsList [pointsList.Count - 1];
+	//			if (isLinesIntersect (lines [i], currentLine))
+	//				return true;
+	//		}
+	//		return false;
+	//	}
 	//    -----------------------------------    
 	//    Following method checks whether given two points are same or not
 	//    -----------------------------------    
-	private bool checkPoints (Vector3 pointA, Vector3 pointB)
-	{
-		return (pointA.x == pointB.x && pointA.y == pointB.y);
-	}
+	//	private bool checkPoints (Vector3 pointA, Vector3 pointB)
+	//	{
+	//		return (pointA.x == pointB.x && pointA.y == pointB.y);
+	//	}
 	//    -----------------------------------    
 	//    Following method checks whether given two line intersect or not
 	//    -----------------------------------    
-	private bool isLinesIntersect (myLine L1, myLine L2)
-	{
-		if (checkPoints (L1.StartPoint, L2.StartPoint) ||
-			checkPoints (L1.StartPoint, L2.EndPoint) ||
-			checkPoints (L1.EndPoint, L2.StartPoint) ||
-			checkPoints (L1.EndPoint, L2.EndPoint))
-			return false;
-
-		return((Mathf.Max (L1.StartPoint.x, L1.EndPoint.x) >= Mathf.Min (L2.StartPoint.x, L2.EndPoint.x)) &&
-			(Mathf.Max (L2.StartPoint.x, L2.EndPoint.x) >= Mathf.Min (L1.StartPoint.x, L1.EndPoint.x)) &&
-			(Mathf.Max (L1.StartPoint.y, L1.EndPoint.y) >= Mathf.Min (L2.StartPoint.y, L2.EndPoint.y)) &&
-			(Mathf.Max (L2.StartPoint.y, L2.EndPoint.y) >= Mathf.Min (L1.StartPoint.y, L1.EndPoint.y))
-		);
-	}
+	//	private bool isLinesIntersect (myLine L1, myLine L2)
+	//	{
+	//		if (checkPoints (L1.StartPoint, L2.StartPoint) ||
+	//			checkPoints (L1.StartPoint, L2.EndPoint) ||
+	//			checkPoints (L1.EndPoint, L2.StartPoint) ||
+	//			checkPoints (L1.EndPoint, L2.EndPoint))
+	//			return false;
+	//
+	//		return((Mathf.Max (L1.StartPoint.x, L1.EndPoint.x) >= Mathf.Min (L2.StartPoint.x, L2.EndPoint.x)) &&
+	//			(Mathf.Max (L2.StartPoint.x, L2.EndPoint.x) >= Mathf.Min (L1.StartPoint.x, L1.EndPoint.x)) &&
+	//			(Mathf.Max (L1.StartPoint.y, L1.EndPoint.y) >= Mathf.Min (L2.StartPoint.y, L2.EndPoint.y)) &&
+	//			(Mathf.Max (L2.StartPoint.y, L2.EndPoint.y) >= Mathf.Min (L1.StartPoint.y, L1.EndPoint.y))
+	//		);
+	//	}
 }
