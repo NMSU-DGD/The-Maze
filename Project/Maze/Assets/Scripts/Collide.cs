700c6fe0df42cf8f69eedd19196bb6e88e1316d4
Sultan Alharthi <salharth@nmsu.edu>
2016-11-15 20:26:38 -0700

    Make the keypad disappear.

--- Project/Maze/Assets/Scripts/Collide.cs
+++ Project/Maze/Assets/Scripts/Collide.cs
@@ -15,7 +15,27 @@ public class Collide : MonoBehaviour {
 		Movement2D player = otherCollider.gameObject.GetComponent<Movement2D> ();
 			Debug.Log("ok");
 			KeyPad.active = true;
-			//Destroy(gameObject);
+	}
+
+	void OnTriggerExit2D(Collider2D otherCollider)
+	{
+		string keyName = gameObject.name;
+		Movement2D player = otherCollider.gameObject.GetComponent<Movement2D>();
+		if (player != null) {
+			if (keyName == "t")
+			{
+				KeyPad.active = false;
+				//Vector3 temp2 = new Vector3(2.27f,2.28f,0.5f);
+				//GameObject.Find ("Main Camera").GetComponent<Camera> ().transform.localScale = temp2;
+				//Vector3 temp = new Vector3(7.4f,20.0f,-2.0f);
+				//GameObject.Find ("Main Camera").GetComponent<Camera> ().transform.position -= temp;
+				//Debug.Log("Plan");
+				//GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
+				//GameObject.Find("Planningmap").GetComponent<Camera>().enabled = false;
+			}
+
+		}
+
 	}
 
 //
