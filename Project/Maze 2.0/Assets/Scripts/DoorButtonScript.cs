3c6696af7272dcdab195b08b842189da037980cf
IAHMED\Ahmed <al_khalaf30@hotmail.com>
2016-11-27 23:32:49 -0700

    Hold the key to open the door.

--- Project/Maze 2.0/Assets/Scripts/DoorButtonScript.cs
+++ Project/Maze 2.0/Assets/Scripts/DoorButtonScript.cs
@@ -6,11 +6,14 @@ public class DoorButtonScript : MonoBehaviour {
 
 	Movement2D player;
 
+
 	void OnTriggerEnter2D(Collider2D otherCollider)
 	{
 		string keyName = gameObject.name;
 		player = otherCollider.gameObject.GetComponent<Movement2D>();
-		if (player != null) {
+
+        Debug.Log(player);
+        if (player != null) {
 			if (keyName == "DoorButton")
 			{
 				GameObject.Find("ButtonDoor01").GetComponent<MeshRenderer>().enabled = false;
---
+++ Project/Maze 2.0/Assets/Scripts/MoveKey.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveKey : MonoBehaviour {
+
+    Vector3 avaPos;
+    GameObject theAvatar;
+    public static bool MovingTheKey = false;
+    // Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+
+        theAvatar = GameObject.Find("Player(Clone)");
+        if (theAvatar != null)
+        {
+            avaPos = theAvatar.transform.position;
+        }
+
+        if (MovingTheKey)
+        {
+
+            this.gameObject.transform.position = new Vector3(avaPos.x, avaPos.y*1.05f, avaPos.z);
+            this.gameObject.transform.rotation = theAvatar.transform.rotation;
+        }
+
+    }
+}
