3bb20fc25fbcc3ecb56628d12380a2b678443523
IAHMED\Ahmed <al_khalaf30@hotmail.com>
2016-12-03 00:52:00 -0700

    Adding a countdown timer

---
+++ Project/Maze 3.1/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    GameObject timer;
+    float targetTime = 60.00f;
+    public static bool timeCon = true;
+    public GUIText theTimer;
+    public GUIText theTime;
+
+    void Start()
+    {
+        timer = GameObject.Find("theTimer");
+        theTime.text = "Timer: ";
+    }
+
+    void Update()
+    {
+        if (targetTime > 0)
+        {
+            targetTime -= Time.deltaTime;
+
+            theTimer.text = targetTime.ToString("00.0");
+            timer.GetComponent<Text>().text = theTime.text +"      "+ theTimer.text;
+
+        }
+
+    }
+
+}
