8d81b0cdab905754b6d671959e67f5a09c9059b2
Sultan Alharthi <salharth@nmsu.edu>
2016-12-03 18:05:20 -0700

    Move from Level 1 to level 2

---
+++ Project/Maze 3.1/Assets/Scripts/OpenLevel2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenLevel2 : MonoBehaviour {
+	PlayerMovement player;
+	PlayerMovement02 player2;
+
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		string keyName = gameObject.name;
+		player = otherCollider.gameObject.GetComponent<PlayerMovement>();
+		player2 = otherCollider.gameObject.GetComponent<PlayerMovement02>();
+		if (player != null) {
+			if (keyName == "End2") {
+				Debug.Log("End");
+				Vector3 temp = new Vector3(-6.76f,2.26f,-1.0f);
+				GameObject.FindGameObjectWithTag ("player1").GetComponent<SpriteRenderer> ().transform.position = temp;
+			}
+	}
+}
+	void OnTriggerExit2D(Collider2D otherCollider)
+	{
+		string keyName = gameObject.name;
+		player = otherCollider.gameObject.GetComponent<PlayerMovement>();
+		player2 = otherCollider.gameObject.GetComponent<PlayerMovement02>();
+		if (player != null) {
+			if (keyName == "End2") {
+				Debug.Log("End");
+				GameObject.Find("Level02").GetComponent<SpriteRenderer>().enabled = true;
+				GameObject.Find("BlackCover02").GetComponent<SpriteRenderer>().enabled = false;
+				GameObject.Find("Lock02").GetComponent<SpriteRenderer>().enabled = false;
+				GameObject.Find("Level2Door").GetComponent<MeshRenderer>().enabled = false;
+				GameObject.Find("Level2Door").GetComponent<BoxCollider2D>().enabled = false;
+				GameObject.Find("BlackCover02.5").GetComponent<SpriteRenderer>().enabled = true;
+				GameObject.Find("Lock02.5").GetComponent<SpriteRenderer>().enabled = true;
+			}
+		}
+	}
+}
